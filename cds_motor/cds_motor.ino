973ce131ace7786ad026ecbfb7ba5e0a1ad68426
unknown <>
2016-06-04 17:10:58 +0900

    광센서로 DC모터 제어.

---
+++ cds_motor/cds_motor.ino
@@ -0,0 +1,20 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(7,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int cdsValue = analogRead(A1);
+  Serial.print("cds =  ");
+  Serial.println(cdsValue);
+  if (cdsValue > 50) {
+	digitalWrite(7, HIGH);
+	Serial.println("MOTER ON (cds > 50)");
+   }
+  else {
+	digitalWrite(7, LOW);
+	Serial.println("LED OFF (cds < 50)");
+  }
+  delay(200); 
+}
